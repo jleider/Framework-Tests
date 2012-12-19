849c7a15f3f65c12ff92c5c851a7c0f5505260cb
Steven Moseley <smoseley@transio.com>
2012-12-19 15:18:12 -0500

    Added zend framework stuff

--- zend/vendor/autoload.php
+++ zend/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitc50b47d691b2b2803d64a083dd8843c6::getLoader();
+return ComposerAutoloaderInitfe243212d5ab0aa16e25d78edbfd86a0::getLoader();
