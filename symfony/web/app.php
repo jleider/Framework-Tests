2045b9cf34dd5f0c4ff2de16a067d4ae612f0433
Justin <justin@justinleider.com>
2012-12-20 11:03:05 -0500

    update tests

--- symfony/web/app.php
+++ symfony/web/app.php
@@ -1,18 +1,17 @@
 <?php
 
-umask(0000);
-
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-
-$loader = new ApcClassLoader('framework-tests-sf2', $loader);
+// Use APC for autoloading to improve performance.
+// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+// with other applications also using APC.
+/*
+$loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
-
+*/
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
